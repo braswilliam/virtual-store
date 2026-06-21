56417d565fffdc7ea9414f651d1ee4bccd6dfcba
William B. Pereira <braswilliam@gmail.com>
2026-06-21 12:26:19 -0300

    virtual store is done

---
+++ view/login.php
@@ -0,0 +1,179 @@
+<!DOCTYPE html>
+<html lang="Pt-BR">
+
+<head>
+    <title>Bro's Mart - Compre com seu Bro!</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <!--Bootstrap CDN-->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+    <!--/Bootstrap CDN-->
+
+    <!--Font Awesome CDN-->
+    <script src="https://kit.fontawesome.com/80ca821bc6.js" crossorigin="anonymous"></script>
+    <!--/Font Awesome CDN-->
+
+    <!--Slik Slider-->
+    <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css" />
+    <!--/Slik Slider-->
+
+    <!--Estilos-->
+    <link rel="stylesheet" href="../css/style.css">
+    <!--/Estilos-->
+
+
+
+
+
+
+
+</head>
+
+<body>
+    <!--Iniciando Header-->
+    <header>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-4 col-sm-12 col-12">
+                    <div class="btn-group">
+                        
+                    </div>
+                </div>
+                <div class="col-md-4 col-12 text-center">
+                    <h2 class="my-md-3 site-title text-white"> Bro's Mart</h2>
+                </div>
+            </div>
+        </div>
+
+       
+    </header>
+    <!--/Header-->
+<br> 
+
+
+    <!--Iniciando o main-->
+    <main>
+    <div id="login">
+        <h3 class="text-center text-white pt-5">Login form</h3>
+        <div class="container">
+            <div id="login-row" class="row justify-content-center align-items-center">
+                <div id="login-column" class="col-md-6">
+                    <div id="login-box" class="col-md-12">
+                        <form id="login-form" class="form" action="" method="post">
+                            <h3 class="text-center text-info">Login</h3>
+                            <div class="form-group">
+                                <label for="username" class="text-info">Username:</label><br>
+                                <input type="text" name="username" id="username" class="form-control">
+                            </div>
+                            <div class="form-group">
+                                <label for="password" class="text-info">Password:</label><br>
+                                <input type="text" name="password" id="password" class="form-control">
+                            </div>
+                            <div class="form-group">
+                                <label for="remember-me" class="text-info"><span>Remember me</span> <span><input id="remember-me" name="remember-me" type="checkbox"></span></label><br>
+                                <input type="submit" name="submit" class="btn btn-info btn-md" value="submit">
+                            </div>
+                            <div id="register-link" class="text-right">
+                                <a href="#" class="text-info">Register here</a>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+        
+
+    </main>
+   
+<br> 
+<br> 
+<br> 
+
+    <footer>
+        <div class="container-fluid px-5">
+            <div class="row">
+                <div class="col-md-4 col-sm-12">
+                    <h4>Contate-nos</h4>
+                    <div class="row pl-md-1 text-secondary">
+                        <div class="col-12">
+                            <i class="fa fa-home px-md-2 "></i>
+                            <small>201 50070 110, Av. Governador Carlos de Lima Cavalcanti, 155 - Boa Vista, Recife - PE, 50070-110</small>
+                        </div>
+                    </div>
+                    <div class="col-12">
+                        <i class="fa fa-paper-plane px-md-2 "></i>
+                        <small>-braswilliam@gmail.com -lucas1999mendes@gmail.com -jgsilvaneto2014@gmail.com </small>
+                    </div>
+                    <div class="col-12">
+                        <i class="fa fa-phone-volume px-md-2 "></i>
+                        <small>+55 81 99704-2985</small>
+                    </div>
+
+                    <div class="row social text-secondary">
+                        <div class="col-12 py-3">
+                            <i class="fab fa-twitter"></i>
+                            <i class="fab fa-facebook"></i>
+                            <i class="fab fa-intagram"></i>
+                            <i class="fab fa-linkedin"></i>
+                            <i class="fab fa-whatsapp"></i>
+                            <i class="fab fa-telegram"></i>
+                            <i class="fab fa-google"></i>
+                            <i class="fab fa-skype"></i>
+                            <i class="fab fa-pinterest"></i>
+                            <i class="fab fa-youtube"></i>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-2 col-sm-12">
+                    <h4>Outros serviços</h4>
+                    <div class="d-flex flex-column">
+                        <small class="pt-0">Recife</small>
+                        <small>Jaboatão</small>
+                        <small>Caruaru</small>
+                        <small>Olinda</small>
+                        <small>Paulista</small>
+                    </div>
+                </div>
+                <div class="col-md-2 col-sm-12">
+                    <h4>Extra Info...</h4>
+                    <div class="d-flex flex-column">
+                        <small class="pt-0">Sobre a Loja</small>
+                        <small>Novas Ofertas</small>
+                        <small>Fale Conosco</small>
+                        <small>Ultimas Notícias</small>
+                        <small>Localização</small>
+                    </div>
+                </div>
+                <div class="col-md-4 follow-us col-sm-12">
+                    <h4>Nos Sigam no Instagram</h4>
+                    <div class="flex flex-row">
+                        <img src="../fotos/256_n.jpg" alt="instagram 1" class="img-fluid">
+                        <img src="../fotos/792_n.jpg" alt="instagram 2" class="img-fluid">
+                        <img src="../fotos/392_n.jpg" alt="instagram 3" class="img-fluid">
+                    </div>
+                    <div class="flex flex-row">
+                        <img src="../fotos/664_n.jpg" alt="instagram 1" class="img-fluid">
+                        <img src="../fotos/088_n.jpg" alt="instagram 2" class="img-fluid">
+                        <img src="../fotos/896_n.jpg" alt="instagram 3" class="img-fluid">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
+
+
+    <!--Bootstrap CDN-->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+    <!--/Bootstrap CDN-->
+    <!--Slik Slider-->
+    <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
+    <!--Slik Slider-->
+    <script src="main.js"></script>
+</body>
+
+</html>
